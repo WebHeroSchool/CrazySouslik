5fc35cd0f5d9e0b59177c9acb9df8893d0fa58b4
ChuVo <cahek764@ya.ru>
2019-04-15 23:06:27 +0300

    added examples works

--- src/scripts/section-works.js
+++ src/scripts/section-works.js
@@ -1,5 +1,21 @@
+// функция показывает текст при наведении на изображение примера работы(сайт/игра)
+
 ( function() {
 
-  console.log('ffff');
+  const elem = document.querySelectorAll('.works__site');
 
-})();
+  elem.forEach( (i) => {
+
+    i.addEventListener( 'mouseover', getNextElem, true );
+    i.addEventListener( 'mouseout', closeNextElem, true );
+
+  }); 
+
+  function getNextElem(e) {
+    e.target.parentNode.nextElementSibling.classList.add( 'text-typing' );
+  }
+  function closeNextElem(e) {
+    e.target.parentNode.nextElementSibling.classList.remove( 'text-typing' ); 
+  }
+  
+}());
