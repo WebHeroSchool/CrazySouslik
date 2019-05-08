249396c6191000fa5fa6f9baa3ee4ba679a598c4
ChuVo <cahek764@ya.ru>
2019-05-08 11:05:51 +0300

    fix preloader +%

--- src/templates/preloader/preloader.js
+++ src/templates/preloader/preloader.js
@@ -2,13 +2,44 @@
 
   document.body.onload = function() {
 
-      setTimeout( function() {
-        const preloader = document.querySelector( '.preloader');
-    
-        if( !preloader.classList.contains( 'done' ) ) {
-          preloader.classList.add( 'done' );
-        }
-      }, 1000);
-    };
+    const preloader = document.querySelector( '.preloader'),
+          progressDisplay = document.querySelector( '.percentDisplay');
+
+    let images = document.images,
+        progress = 0,
+        imagesTotalCount = images.length,
+        imagesLoadedCount = 0,
+        imageClone = new Image();
+        
+
+    for( let i = 0; i < imagesTotalCount; i++ ) {
+
+      imageClone.src = images[i].src;
+      imageClone.onload = imageLoaded;
+      imageClone.onerror = imageLoaded();
+    }
+
+    function imageLoaded() {
+
+      progress = (( (100 / imagesTotalCount) * imagesLoadedCount ) << 0 );
+
+      progressDisplay.innerHTML = progress + '%';
+      imagesLoadedCount++;
+
+      console.log( progress);
+
+      if ( imagesLoadedCount >= imagesTotalCount ) {
+      
+        setTimeout( function() {          
+            
+          if( !preloader.classList.contains( 'preloader__done' ) ) {
+            preloader.classList.add( 'preloader__done' );
+          }
+        }, 1000);
+      }
+    }
+  }
+
+  
 
 }());
